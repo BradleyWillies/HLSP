c8e89794796babd18975e857c80ec1e83c21bb6a
Bradley Willies <bradley.willies@gmail.com>
2022-10-20 23:21:45 +0100

    index JS Validation

---
+++ WebContent/index.js
@@ -0,0 +1,32 @@
+const errorElement = document.getElementById('errorElement')
+const form = document.getElementById('registerForm')
+const password = document.getElementById('passwordInput')
+
+form.addEventListener('submit', (e) => {
+	let errorMessages = []
+	
+	let passMinLength = 8
+	if (password.value.length < passMinLength) {
+		errorMessages.push('Password must be longer than ' + passMinLength + ' characters')
+	}
+	
+	let regex = /^(?=.*[\d])(?=.*[!@#$%^&*])[\w!@#$%^&*]{6,16}$/
+	if (!regex.test(password.value)) {
+		errorMessages.push('Password must contain at least one number and one special character')
+	}
+	
+	if (errorMessages.length > 0) {
+    	e.preventDefault()
+
+		errorMessages.forEach (msg => {
+			msg = '<div class="alert alert-danger" role="alert">' + msg + '</div>'
+			console.log(msg)
+		})
+		
+		for (let i = 0; i < errorMessages.length; i++) {
+			errorMessages[i] = '<div class="alert alert-danger" role="alert">' + errorMessages[i] + '</div>'
+		}
+		
+    	errorElement.innerHTML = errorMessages.join('\n')
+  	}
+})
